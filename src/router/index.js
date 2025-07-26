cf97c6bc914adfdca015047d515c9b0e12165314
chetracoding <hongchetra12@gmail.com>
2025-07-27 00:07:40 +0700

    chore(app): setup vuejs app

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router/auto'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/pages/index.vue'),
+    },
+  ],
+})
+
+export default router
